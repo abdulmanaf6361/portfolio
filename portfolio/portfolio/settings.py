d87d7977caee021799fe4f4c9fefcba3c26fde6b
Abdul Manaf B <abdulmanaf6361@gmail.com>
2024-08-01 20:14:47 +0530

    database fix

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -5,6 +5,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
