5adbf778dff4a97a5856038c4d0a43fdd682f87a
Abdul Manaf B <abdulmanaf6361@gmail.com>
2024-08-03 22:53:22 +0530

    logging added

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -144,3 +144,22 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
