09cde1576f2e3ffeabb6dd426de7161cfc637bd1
Abdul Manaf B <abdulmanaf6361@gmail.com>
2024-08-05 22:47:21 +0530

    adminmodify

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('search/', search, name='search'),
 
     path('dashboard/', include('dashboard.urls')),
-    path('admin/', admin.site.urls),
+    path('kasfhiufhw8ru-938240r3jir/', admin.site.urls),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
